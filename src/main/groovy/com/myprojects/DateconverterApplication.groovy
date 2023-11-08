86fa4b3732edaca42b46b4a2c51d64b84209c54e
Sowjanya Tirumala <sowjanyatirumala@outlook.com>
2023-11-07 21:48:53 -0600

    Initial commit

---
+++ src/main/groovy/com/myprojects/DateconverterApplication.groovy
@@ -0,0 +1,13 @@
+package com.myprojects
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DateconverterApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(DateconverterApplication, args)
+	}
+
+}
